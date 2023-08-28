5e161cc4c9f7eb7c58decd513152baeff52c8105
taho7766 <wjtw9802@icloud.com>
2023-08-27 18:49:18 -0600

    fixed typo at line 13 and added return form for editing projects at EditProject.js

--- frontend/src/views/EditProject.js
+++ frontend/src/views/EditProject.js
@@ -10,7 +10,7 @@ const EditProject = (props) => {
     });
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`)
+        fetch(`http://localhost:5000/projects/${props.match.params.id}`)
             .then(response => response.json())
             .then(data => setProject(data))
             .catch(error => console.error('Error:', error));
@@ -40,4 +40,35 @@ const EditProject = (props) => {
                 props.history.push(`/projects/${props.match.params.id}`);
             });
     }
+
+    return (
+        <div>
+            <h1>Edit Project</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Title</label>
+                    <input type='text' name='title' value={project.title} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Description</label>
+                    <textarea name='description' value={project.description} onChange={handleChange} required></textarea>
+                </div>
+                <div>
+                    <label>Technologies</label>
+                    <input type='text' name='technologies' value={project.technologies} onChange={handleChange} required />
+                </div>
+                <div>
+                    <label>Image URL</label>
+                    <input type='text' name='imageURL' value={project.imageURL} onChange={handleChange} />
+                </div>
+                <div>
+                    <lable>Project URL</lable>
+                    <input type='url' name='projectURL' value={project.projectURL} onChange={handleChange} required />
+                </div>
+                <button type='submit'>Update Project</button>
+            </form>
+        </div>
+    );
 }
+
+export default EditProject;
